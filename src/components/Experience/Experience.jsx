b4586190a76928a7f84f5e7360080f2892922933
Shubham Patel <shubhampatel872005@gmail.com>
2025-09-26 19:08:24 +0530

    fixing some bug

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -10,7 +10,7 @@ const achivement = () => {
       {/* Section Title */}
       <div className="text-center mb-16">
         <h2 className="text-4xl font-bold text-white">
-          Achievements & <span className="text-green-600">& Certifications</span>
+          Achievements &<span className="text-green-600">Certifications</span>
         </h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
         <p className="text-gray-400 mt-4 text-lg font-semibold">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   const menuItems = [
     { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
-    { id: "experience", label: "Experience" },
+    { id: "achivement", label: "Achivement/Certifications" },
     { id: "work", label: "Projects" },
     { id: "education", label: "Education" },
   ];
