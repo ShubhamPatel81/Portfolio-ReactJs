7922cc5033bda3a63d7a2a2a0587059ee28cab53
Shubham Patel <shubhampatel872005@gmail.com>
2025-10-28 21:23:10 +0530

    changin resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/14TMz2FPaChNcVTRvQEimXr0nQbN3U3Ym/view?usp=sharing"
+            href="https://drive.google.com/file/d/1DmmYj9CJ11AypIH1wWI__rSWitBlo7aL/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
